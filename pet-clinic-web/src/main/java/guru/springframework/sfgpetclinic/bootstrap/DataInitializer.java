c44d6b3df3851fddd20066bdb7fca8b42ddb026f
Paul K <pik@umich.edu>
2020-03-22 22:09:14 +0100

    Initialize Data. Closes #14

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataInitializer.java
@@ -0,0 +1,73 @@
+package guru.springframework.sfgpetclinic.bootstrap;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.VetService;
+import guru.springframework.sfgpetclinic.services.impl.map.OwnerServiceMap;
+import guru.springframework.sfgpetclinic.services.impl.map.PetServiceMap;
+import guru.springframework.sfgpetclinic.services.impl.map.VetServiceMap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component //This becomes Spring bean, going to be registered, and will run
+public class DataInitializer implements CommandLineRunner {
+	private final OwnerService ownerService;
+	private final VetService vetService;
+	private final PetService petService;
+
+	public DataInitializer() {
+		ownerService = new OwnerServiceMap();
+		vetService = new VetServiceMap();
+		petService = new PetServiceMap();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+
+		Owner owner1 = new Owner();
+		owner1.setId(1L);
+		owner1.setFirstName("Michael");
+		owner1.setLastName("Weston");
+//		owner1.setAddress("123 Brickerel");
+//		owner1.setCity("Miami");
+//		owner1.setTelephone("1231231234");
+
+
+		ownerService.save(owner1);
+
+		Owner owner2 = new Owner();
+		owner1.setId(2L);
+		owner2.setFirstName("Fiona");
+		owner2.setLastName("Glenanne");
+//		owner2.setAddress("123 Brickerel");
+//		owner2.setCity("Miami");
+//		owner2.setTelephone("1231231234");
+
+
+		ownerService.save(owner2);
+
+		System.out.println("Loaded Owners....");
+
+
+		Vet vet1 = new Vet();
+		vet1.setId(1L);
+		vet1.setFirstName("Sam");
+		vet1.setLastName("Axe");
+//		vet1.getSpecialities().add(savedRadiology);
+
+		vetService.save(vet1);
+
+		Vet vet2 = new Vet();
+		vet2.setId(2L);
+		vet2.setFirstName("Jessie");
+		vet2.setLastName("Porter");
+//		vet2.getSpecialities().add(savedSurgery);
+
+		vetService.save(vet2);
+
+		System.out.println("Loaded Vets....");
+	}
+}
