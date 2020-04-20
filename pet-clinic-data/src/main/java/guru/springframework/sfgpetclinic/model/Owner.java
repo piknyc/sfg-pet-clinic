548b08f61ae5cbfcde0921d448d5f8d461eb1400
Paul K <pik@umich.edu>
2020-04-20 12:20:19 +0200

    Fix save bug closes #61

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -41,6 +41,16 @@ public class Owner extends Person {
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
 	private Set<Pet> pets = new HashSet<>();
 
+	/**
+	 * Return the Pet with the given name, or null if none found for this Owner.
+	 *
+	 * @param name to test
+	 * @return true if pet name is already in use
+	 */
+	public Pet getPet(String name) {
+		return getPet(name, false);
+	}
+
 
 	public Pet getPet(String name, boolean ignoreNew) {
 		name = name.toLowerCase();
